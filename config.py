9523fc6f7d03394ade4e3a2722e8af18d2009346
zx3224649 <74590514+zx3224649@users.noreply.github.com>
2023-01-27 23:11:33 +0800

    Update config.py

--- config.py
+++ config.py
@@ -18,10 +18,10 @@ DEFAULT_HEADERS = {
 """
 调试用 COOKIE
 """
-TEST_COOKIE = ''
+TEST_COOKIE = '__ckguid=qTB6kBbw5bRPcK2cUJq1Ca; device_id=2130706433167483142663421764fdff9bed875af8b6f1ef972df7f7ec; homepage_sug=d; r_sort_type=score; _zdmA.vid=*; sajssdk_2015_cross_new_user=1; Hm_lvt_9b7ac3d38f30fe89ff0b8a0546904e58=1674831429; footer_floating_layer=0; ad_date=27; bannerCounter=%5B%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%5D; ad_json_feed=%7B%7D; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22185f3bb799e163-0c6b85966d362d-26021051-1327104-185f3bb799f169%22%2C%22first_id%22%3A%22%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%7D%2C%22%24device_id%22%3A%22185f3bb799e163-0c6b85966d362d-26021051-1327104-185f3bb799f169%22%7D; sess=BA-gKmHOVUlEfLmgotQVhVh%2BN6yXTkygQXLxBgq7t1NWqkTeFkG2%2FCovNx8zawuuGHfazt3tovaICNpVivBeSAGeiBP5i0nALNAQDkrBiVQhtdABWm2S%2FZmiyPb; user=user%3A7107891882%7C7107891882; _zdmA.uid=ZDMA.oUmRILBsI.1674831471.2419200; smzdm_id=7107891882; Hm_lpvt_9b7ac3d38f30fe89ff0b8a0546904e58=1674831471; amvid=ebf44b3a92f72e0aa7110dbfa46c6696; _zdmA.time=1674831472821.0.https%3A%2F%2Fwww.smzdm.com%2F'
 
 
 """
 调试用 SERVERCHAN_SECRETKEY
 """
-SERVERCHAN_SECRETKEY = ''
+SERVERCHAN_SECRETKEY = 'SCT2158T0jm7REjfQ3gsmtDITh6ZPCLz'
